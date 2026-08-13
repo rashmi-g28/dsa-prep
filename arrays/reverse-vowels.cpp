0a298a3705d02501b27f08c78753726e4233e1f0
rashmigaonkar28 <rashmigaonkar305@gmail.com>
2026-08-13 18:51:02 +0530

    Day 32: Product of Array Except Self

--- arrays/reverse-vowels.cpp
+++ arrays/reverse-vowels.cpp
@@ -30,3 +30,22 @@ Constraints:
 1 <= s.length <= 3 * 105
 s consist of printable ASCII characters.
 */
+
+    string reverseVowels(string s) {
+        vector<char> vowels;
+        int n=s.length();
+        for(char c : s){
+            if(isalpha(c) && (c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')){
+                vowels.push_back(c);
+            }
+        }
+        int m=vowels.size();
+        m--;
+        for(int i=0;i<n && m>=0;i++){
+            if(isalpha(s[i]) && (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')){
+                s[i]=vowels[m];
+                m--;
+            }
+        }
+        return s;
+    }

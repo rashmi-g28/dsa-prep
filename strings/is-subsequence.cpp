d54894015c7f1112864f30557fa0b6363e7bd04f
rashmigaonkar28 <rashmigaonkar305@gmail.com>
2026-08-17 20:35:44 +0530

    Day 36: Unique Paths

--- strings/is-subsequence.cpp
+++ strings/is-subsequence.cpp
@@ -27,7 +27,7 @@ s and t consist only of lowercase English letters.
 Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
 */
 
-    bool isSubsequence(string s, string t) {
+bool isSubsequence(string s, string t) {
         int n=s.length();
         int m=t.length();
         int i=0,j=0;
